e20af89ff0656f8bd0b76c7c416bcd70a771fe95
Breno Rodrigues Guimaraes <brenorg@gmail.com>
2021-07-27 20:26:39 -0300

    Update script to convert to image. Use png and split pages before converting

---
+++ convert_pdf_to_image.py
@@ -0,0 +1,21 @@
+import json
+import time
+import requests
+import concurrent.futures
+import os
+import subprocess
+
+def convert_pdf_to_image(from_f, to_f):
+    subprocess.run("./pdf2image {} {}".format(from_f, to_f), shell=True)
+
+pdfdir = 'database/pdfs'
+imagedir = 'database/images'
+
+subprocess.run("mkdir -p {}".format(imagedir), shell=True)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+    for pdf in os.listdir(pdfdir):
+        from_f = os.path.join(pdfdir, pdf)
+        to_f = os.path.join(imagedir, os.path.splitext(pdf)[0])
+        if not os.path.exists(to_f):
+            executor.submit(convert_pdf_to_image, from_f, to_f)
